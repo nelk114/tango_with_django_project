7f6c6ae53109fd614d30b83b9435430d79d8b760
Bernhard Nicolás Ersfeld Mandujano <2202252e@student.gla.ac.uk>
2025-01-22 18:48:31 +0000

    Add about Link to index Page

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(r):
-	return HttpResponse('Rango says hey there partner!')
+	return HttpResponse('Rango says hey there partner!<br><a href=\'/rango/about/\'>About</a>')
 
 def about(r):
 	return HttpResponse('Rango says here is the about page.')
