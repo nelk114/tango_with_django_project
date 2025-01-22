2b516aeef116099bc17d2854f84b99cde49999b6
Bernhard Nicolás Ersfeld Mandujano <2202252e@student.gla.ac.uk>
2025-01-22 18:49:22 +0000

    Link back

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ def index(r):
 	return HttpResponse('Rango says hey there partner!<br><a href=\'/rango/about/\'>About</a>')
 
 def about(r):
-	return HttpResponse('Rango says here is the about page.')
+	return HttpResponse('Rango says here is the about page.<br><a href=\'/rango/\'>Index</a>')
