80fd5fc6e4be48a8628176e63dfa5a2169debf24
Bernhard Nicolás Ersfeld Mandujano <2202252e@student.gla.ac.uk>
2025-01-31 09:34:43 +0000

    Pass top Categories to index view

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(r):
-	context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+	context_dict={
+		'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!',
+		'categories':Category.objects.order_by('-likes')[:5],
+		}
 	return render(r,'rango/index.html',context=context_dict)
 
 def about(r):
