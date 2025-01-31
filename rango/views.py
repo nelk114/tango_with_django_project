559ad3ed928be337062a3bb2a70d4ea9c88319a8
Bernhard Nicolás Ersfeld Mandujano <2202252e@student.gla.ac.uk>
2025-01-31 14:34:31 +0000

    Register Category View
    Won't work yet as the referred‐to template doesn't exist yet (ꝥ's deferred until the next step)
    Continuous‐Integration‐style ‘keep it working at each step’ is clearly not a priority for this textbook

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category,Page
 
 def index(r):
 	context_dict={
@@ -12,3 +12,12 @@ def index(r):
 def about(r):
 	ctx={'nm':'Bernhard Nicolás Ersfeld Mandujano'}
 	return render(r,'rango/about.html',context=ctx)
+
+def show_category(r,category_name_slug):	#Slug as 2nd param
+	ctx={}
+	try:
+		cat=Category.objects.get(slug=category_name_slug)
+		ctx['category']=cat;ctx['pages']=Page.objects.filter(category=cat)
+	except Category.DoesNotExist:
+		ctx['category']=None;ctx['pages']=None
+	return render(r,'rango/category.html',context=ctx)
