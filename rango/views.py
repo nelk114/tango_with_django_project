b0959c0d27de17efb3f5a1e07f2e546ea938dad9
Bernhard Nicolás Ersfeld Mandujano <2202252e@student.gla.ac.uk>
2025-02-04 17:41:35 +0000

    Add Registration View
    The difference in coding style compared to the previous views is interesting(?)
    In particular I somewhat prefer the early‐exit approach of the others
    (could've got away with a one‐liner in the outer else),
    especially since all the values passed to the context dict are different.
    Saves us typing out the keys twice I suppose…

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from rango.models import Category,Page
-from rango.forms import CategoryForm,PageForm
+from rango.forms import CategoryForm,PageForm,UserForm,UserProfileForm
 
 def index(r):
 	context_dict={
@@ -48,3 +48,20 @@ def add_page(r,category_name_slug):
 				return redirect(reverse('rango:show_category',kwargs={'category_name_slug':category_name_slug}))
 		else:print(f.errors)
 	return render(r,'rango/add_page.html',context={'form':f,'category':cat})
+
+def register(r):
+	reg=False
+	if r.method=='POST':
+		uf=UserForm(r.POST);pf=UserProfileForm(r.POST)
+		if uf.is_valid() and pf.is_valid():
+			l=uf.save()
+			l.set_password(l.password);l.save()
+			p=pf.save(commit=False)
+			p.user=l
+			if 'picture' in r.FILES:p.picture=r.FILES['picture']
+			p.save()
+			reg=True
+		else:print(uf.errors,pf.errors)
+	else:
+		uf=UserForm();pf=UserProfileForm()
+	return render(r,'rango/register.html',context={'user_form':uf,'profile_form':pf,'registered':reg})
